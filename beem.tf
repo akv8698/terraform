1a6dff7f6ae3a44dec4a2127f1937fe2923da74e
akv8698 <akrihnavamsi2016@gmail.com>
2023-06-08 20:19:53 +0530

    teeraform commit

---
+++ beem.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAVXOWXZPM5IQVFC6X"
+secret_key ="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"
+}
+
+
+
+resource "aws_instance" "hari"{
+ami = "ami-0f5ee92e2d63afc18"
+instance_type  = "t2.micro"
+key_name  ="9amapril"
+tags      ={
+Name      ="tom"
+}
+}
