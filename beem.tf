6aa9d6d0c075cb8259fd969ec4d9960891c0fdad
akv8698 <130883347+akv8698@users.noreply.github.com>
2023-06-09 18:57:39 +0530

    Update beem.tf

--- beem.tf
+++ beem.tf
@@ -6,11 +6,11 @@ secret_key ="n9+7se5sSXxO2UGBeETk6CZSW9jNxxNSwb948HsS"
 
 
 
-resource "aws_instance" "hari"{
+resource "aws_instance" "hari77"{
 ami = "ami-0f5ee92e2d63afc18"
 instance_type  = "t2.micro"
 key_name  ="9amapril"
 tags      ={
-Name      ="tom"
+Name      ="tomd"
 }
 }
